03ef3db5e0bbac721727f0189c1325ec9fa554c8
JoaoDiogoF <diogo.ferreira@hotmail.de>
2018-03-13 17:26:19 +0100

    What is this class for? Have deleted it, if you want to use it please contact me.

--- src/main/java/ch/ntb/jass/common/proto/MoveStatusMessage.java
+++
@@ -1,7 +0,0 @@
-package ch.ntb.jass.common.proto;
-
-// WTF ist das f�r eine Message?
-public class MoveStatusMessage extends ToPlayerMessage {
-    public boolean valid;
-    public String erroMessage;
-}
