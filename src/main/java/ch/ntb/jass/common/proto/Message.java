afa4ad3e70180afcc28c16e1ee243d6701dba53d
Samuel Oggier <samuel.oggier@gmail.com>
2018-03-13 15:30:03 +0100

    use Jackson for serialization (one object per line)

--- src/main/java/ch/ntb/jass/common/proto/Message.java
+++ src/main/java/ch/ntb/jass/common/proto/Message.java
@@ -1,21 +1,37 @@
 package ch.ntb.jass.common.proto;
 
+import ch.ntb.jass.common.proto.server_info_messages.TurnInfoMessage;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import java.util.Objects;
 
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "@type"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ToPlayerMessage.class, name = "serverMessage"),
+        @JsonSubTypes.Type(value = ToServerMessage.class, name = "clientMessage"),
+        @JsonSubTypes.Type(value = TurnInfoMessage.class, name = "turnInfo")
+})
 public class Message {
-    public String type;
+    /**
+     * An id defined by the sender, which may be used in related responses
+     */
+    public String id;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Message message = (Message) o;
-        return Objects.equals(type, message.type);
+        return Objects.equals(id, message.id);
     }
 
     @Override
     public int hashCode() {
-
-        return Objects.hash(type);
+        return Objects.hash(id);
     }
 }
