8ad866008597d9f05848c9f70fe1b56524c93a0a
Maurus Calzaferri <Maurus Calzaferri@DESKTOP-IRK1TVH.home>
2018-03-28 19:28:30 +0200

    Added id and equals method for easy comparison.

--- src/main/java/ch/ntb/jass/common/entities/PlayerEntity.java
+++ src/main/java/ch/ntb/jass/common/entities/PlayerEntity.java
@@ -1,5 +1,18 @@
 package ch.ntb.jass.common.entities;
 
 public class PlayerEntity {
-
+	public String name;
+	public TeamEntity team;
+	public SeatEntity seat;
+	public boolean isBot;
+	public int id;
+	
+	//Methods
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof PlayerEntity)
+			return this.id == ((PlayerEntity)obj).id;
+		else
+			return super.equals(obj);
+	}
 }
--- src/main/java/ch/ntb/jass/common/entities/TeamEntity.java
+++ src/main/java/ch/ntb/jass/common/entities/TeamEntity.java
@@ -3,4 +3,14 @@ package ch.ntb.jass.common.entities;
 public class TeamEntity {
 	public PlayerEntity player1;
 	public PlayerEntity player2;
+	public int teamId;
+	
+	//Methods
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof TeamEntity)
+			return this.teamId == ((TeamEntity)obj).teamId;
+		else
+			return super.equals(obj);
+	}
 }
