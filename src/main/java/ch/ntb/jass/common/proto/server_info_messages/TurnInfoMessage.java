4cf7608644ef8bac3ebccfc23175e6f7c878185b
Maurus Calzaferri <Maurus Calzaferri@DESKTOP-IRK1TVH.home>
2018-03-28 19:47:48 +0200

    Added field player for easier program control.

--- src/main/java/ch/ntb/jass/common/proto/server_info_messages/TurnInfoMessage.java
+++ src/main/java/ch/ntb/jass/common/proto/server_info_messages/TurnInfoMessage.java
@@ -5,10 +5,12 @@ import ch.ntb.jass.common.proto.ToPlayerMessage;
 
 /**
  * This is a information message sent to all players.
- * This message is sent when a card is player, it contains the information of the card played.
+ * This message is sent when a card is played, it contains the information of the card played.
  * @param card played card
+ * @param player Player who played the card
  */
 
 public class TurnInfoMessage extends ToPlayerMessage {
     public CardEntity laidCard;
+    public PlayerEntity player;
 }
