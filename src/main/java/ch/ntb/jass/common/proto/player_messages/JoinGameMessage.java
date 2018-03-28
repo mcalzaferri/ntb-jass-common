ea7499aceff958b291095565ee36de62db905e96
Maurus Calzaferri <Maurus Calzaferri@DESKTOP-IRK1TVH.home>
2018-03-28 23:14:02 +0200

    Added playerData to JoinGameMessage for visualisation of player names

--- src/main/java/ch/ntb/jass/common/proto/player_messages/JoinGameMessage.java
+++ src/main/java/ch/ntb/jass/common/proto/player_messages/JoinGameMessage.java
@@ -1,8 +1,12 @@
 package ch.ntb.jass.common.proto.player_messages;
 
+import ch.ntb.jass.common.entities.PlayerEntity;
 import ch.ntb.jass.common.proto.ToServerMessage;
 
 /**
  * This Message is sent to the Server when the player wishes to join the Game
+ * @param playerData Contains some Data about the player. playerData.id and playerData.seatNr have no purpose here.
  */
-public class JoinGameMessage extends ToServerMessage {}
+public class JoinGameMessage extends ToServerMessage {
+	public PlayerEntity playerData;
+}
