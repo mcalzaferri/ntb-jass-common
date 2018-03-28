ad344d51af52bb303ea670ba7b8053177c6cbacb
Maurus Calzaferri <Maurus Calzaferri@DESKTOP-IRK1TVH.home>
2018-03-28 19:25:39 +0200

    Added a Dictionary to the ScoreEntity for easy data access.
    See comments for more information

--- src/main/java/ch/ntb/jass/common/entities/ScoreEntity.java
+++ src/main/java/ch/ntb/jass/common/entities/ScoreEntity.java
@@ -1,5 +1,10 @@
 package ch.ntb.jass.common.entities;
 
-public class ScoreEntity {
+import java.util.Dictionary;
 
+public class ScoreEntity {
+	//Dictionary of <Team, Score>
+	//Use get(TeamEntity key) to obtain the score of one team
+	//TODO maybe only use id field (int) of TeamEntity instead of TeamEntiy for less data transmission
+	Dictionary<TeamEntity, Integer> scores;
 }
