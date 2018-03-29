316c596fd9cf8366670be0365761df1110a13857
mcalzaferri <mcalzaferri@pcklz222.ntb.ch>
2018-03-29 18:57:17 +0200

    Renamed JoinGameMessage -> JoinLobbyMessage for clarification

--- src/main/java/ch/ntb/jass/common/proto/player_messages/JoinLobbyMessage.java
+++ src/main/java/ch/ntb/jass/common/proto/player_messages/JoinLobbyMessage.java
@@ -4,9 +4,9 @@ import ch.ntb.jass.common.entities.PlayerEntity;
 import ch.ntb.jass.common.proto.ToServerMessage;
 
 /**
- * This Message is sent to the Server when the player wishes to join the Game
+ * This Message is sent to the Server when the player wishes to join the Lobby (This is the when the player wants to connect to the server)
  * @param playerData Contains some Data about the player. playerData.id and playerData.seatNr have no purpose here.
  */
-public class JoinGameMessage extends ToServerMessage {
+public class JoinLobbyMessage extends ToServerMessage {
 	public PlayerEntity playerData;
 }
