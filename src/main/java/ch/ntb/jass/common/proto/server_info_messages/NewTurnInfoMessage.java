aa5cadbf90794252acb65b8adf6195aefdf50389
Maurus Calzaferri <Maurus Calzaferri@192.168.1.107>
2018-03-30 21:15:27 +0200

    Replaced ChooseWiisMessage with field in NewTurnInfoMessage
    - ChooseWiisMessage
    + Field selectWeis in NewTurnInfoMessage

--- src/main/java/ch/ntb/jass/common/proto/server_info_messages/NewTurnInfoMessage.java
+++ src/main/java/ch/ntb/jass/common/proto/server_info_messages/NewTurnInfoMessage.java
@@ -8,8 +8,10 @@ import ch.ntb.jass.common.proto.ToPlayerMessage;
  * Message that defines who's playing next
  * All players are required to check if they are the next player.
  * @param nextPlayer. The player who has to choose a card
+ * @param selectWeis. If true the player can select his wiis in this turn
  */
 
 public class NewTurnInfoMessage extends ToPlayerMessage {
     public PlayerEntity nextPlayer;
+    public boolean selectWeis;
 }
