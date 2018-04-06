fe09513a72ee095e195e4959f93a1a1e7b5d36ae
JoaoDiogoF <diogo.ferreira@hotmail.de>
2018-04-06 17:38:58 +0200

    new StichInfoMessage which extends TurnInfoMessage to declare who won the "Stich".

---
+++ src/main/java/ch/ntb/jass/common/proto/server_info_messages/StichInfoMessage.java
@@ -0,0 +1,7 @@
+package ch.ntb.jass.common.proto.server_info_messages;
+
+import ch.ntb.jass.common.entities.PlayerEntity;
+
+public class StichInfoMessage extends TurnInfoMessage{
+	PlayerEntity playerWhoWonStich;
+}
